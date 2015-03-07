edf9a6dc9dc74eb44a463ed810afddf73c15f524
Kun Lin <dnwk@users.noreply.github.com>
2015-03-06 18:00:27 -0800

    MSC-additional comments
    Clean MSIE related code. Move login screen to function; Add login button overriding.

--- Login_Script/heading.js
+++ Login_Script/heading.js
@@ -1,45 +1,36 @@
-
-function msieversion() {
-        var ua = window.navigator.userAgent;
-        var msie = ua.indexOf("MSIE ");
-        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))    {
-           return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)));
-		}else{            
-           return false;
-		}
-   return false;
-}
-function getURLParameter(name) {
-  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
-}
-//login
-
-$(document).ready(function() {
-	//workaround for IE
-if(msieversion()!= false && getURLParameter('fromLogin')=='true'&&$.PRIMO.session.user.isLoggedIn()==false) location.reload();
-
-if($.PRIMO.session.user.isLoggedIn()==false&&$.cookie(jQuery.PRIMO.session.user.id)!='ignored'){
-		$.magnificPopup.open({
+function loginscreen(){
+	$.magnificPopup.open({
 		items: {
-		  src: '<div class="white-popup"><div class="logostage"><img src="http://www.whitman.edu/penrose/library/images/PenroseLibraryLogo.png" alt="library logo" style="width:50%" /></div><div class="contentstage"><p><a href="http://'+window.location.hostname+'/primo_library/libweb/action/login.do?loginFn=signin&vid=WHITC&targetURL='+document.location+'&fromLogin=true">Whitman College students, faculty, and staff</a><br/><span class="popup-description">Log in for expanded access to electronic materials and Summit/ILL request options.</span></p><p><a href="https://'+window.location.hostname+'/pds?func=load-login&amp;calling_system=primo&amp;institute=01ALLIANCE_WHITC&amp;PDS_HANDLE=&amp;url=http://'+window.location.hostname+'/primo_library/libweb/action/login.do?targetURL='+document.location+'">Registered visiting patrons</a></p><p><a href="#" onclick="$.magnificPopup.close()">Continue as guest</a></p><p><span class="popup-description">For assistance, contact the Penrose Library Circulation Desk (509-527-5192). &nbsp; </span></p></div>',
+		  src: '<div class="white-popup"><div class="logostage"><img src="http://www.whitman.edu/penrose/library/images/PenroseLibraryLogo.png" alt="library logo" style="width:50%" /></div><div class="contentstage"><p><a href="http://'+window.location.hostname+'/primo_library/libweb/action/login.do?loginFn=signin&vid=WHITC&targetURL='+document.location+'&fromLogin=true">Whitman College students, faculty, and staff</a><br/><span class="popup-description">Log in for expanded access to electronic materials and Summit/ILL request options.</span></p><p><a href="https://alliance-primo.hosted.exlibrisgroup.com/pds?func=load-login&amp;calling_system=primo&amp;institute=01ALLIANCE_WHITC&amp;PDS_HANDLE=&amp;url=http://'+window.location.hostname+'/primo_library/libweb/action/login.do?targetURL='+document.location+'">Other registered patrons</a><br/><span class="popup-description">Including retired faculty/staff, alumni, and community borrowers</span></p><p><a href="#" onclick="$.magnificPopup.close()">Continue as guest</a></p><p><span class="popup-description">For assistance, contact the Penrose Library Circulation Desk (509-527-5192). &nbsp; </span></p></div>',
 		  type: 'inline',
 		  closeBtnInside: true
 		},
 		callbacks: {
 			close: function() {
-				$('#exlidMainMenuTile').notify("Login to access more content.",{ elementPosition:"bottom right" , autoHide:false, arrowShow: false});
 				$.cookie(jQuery.PRIMO.session.user.id, 'ignored' , { expires:1 });
-		}
-	  }//callback
+				//optional
+				$('#exlidMainMenuTile').notify("Login to access more content.",{ elementPosition:"bottom right" , autoHide:false, arrowShow: false});
+			}
+		}//callback
 	});
+	
+}
+$(document).ready(function() {
+
+if($.PRIMO.session.user.isLoggedIn()==false&&$.cookie(jQuery.PRIMO.session.user.id)!='ignored'){
+		loginscreen();
 }//if
 		
+//optional		
 if(jQuery.PRIMO.session.user.isLoggedIn()==false&&$.cookie(jQuery.PRIMO.session.user.id)=='ignored'){
 		$('#exlidMainMenuTile').notify("Login to access more content.",{ elementPosition:"bottom right" , autoHide:false, arrowShow: false});
 }
-//"ask us" trigger FreshDesk		
-$('a.EXLMainMenuITEMask_us').click(function(){
-			FreshWidget.show();
-	})
-//FreshDesk
-FreshWidget.init("", {"queryString": "&widgetType=popup&formTitle=Ask+Us+&attachFile=no&searchArea=no", "widgetType": "popup", "buttonType": "text", "buttonText": "Ask Us", "buttonColor": "white", "buttonBg": "#0075b0", "alignment": "2", "offset": "250px", "formHeight": "320px", "url": "https://penroselibrary.freshdesk.com"} );
+
+//Customize Login on the upper right
+$('#exlidSignIn').html('<a onclick="loginscreen();">Sign in</a>');
+//overrite login buttons
+$('#exlidSignIn').html('<a href=\'#\' onclick="loginscreen();">Sign in</a>');
+$( document ).ajaxComplete(function() {
+	$('em.EXLTabHeaderContentAdditionalDataLineSignIn').html('<a  href=\'#\' onclick="loginscreen();">Sign-in for more options</a>');
+});
+});
